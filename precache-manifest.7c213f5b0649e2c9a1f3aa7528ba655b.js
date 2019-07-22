cff83d3bff31c82200b6a05ad5c2b9674fb6ed53
Sebastian Pellitero <sebastian.pellitero@gmail.com>
2019-07-22 03:31:27 -0300

    Updates

---
+++ precache-manifest.7c213f5b0649e2c9a1f3aa7528ba655b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c128d524a835ccb836f34bfaf9ab8f4",
+    "url": "/sizeandshape/index.html"
+  },
+  {
+    "revision": "53199c7b258a05772f26",
+    "url": "/sizeandshape/static/js/2.86b8145c.chunk.js"
+  },
+  {
+    "revision": "fda4ef59f25ee0883a8b",
+    "url": "/sizeandshape/static/js/main.9292815b.chunk.js"
+  },
+  {
+    "revision": "6a7eb0014177cd6746d0",
+    "url": "/sizeandshape/static/js/runtime~main.6b44906d.js"
+  },
+  {
+    "revision": "ecafd7c9346d9d4cd4fca8284089aa9b",
+    "url": "/sizeandshape/static/media/center.ecafd7c9.svg"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "/sizeandshape/static/media/list.cbd8b5a8.svg"
+  },
+  {
+    "revision": "2be41fd59ecb1161877bf0a4d28dfede",
+    "url": "/sizeandshape/static/media/reset.2be41fd5.svg"
+  },
+  {
+    "revision": "e249914e9d3c8c92e0059c063c950353",
+    "url": "/sizeandshape/static/media/tick.e249914e.svg"
+  }
+]);
